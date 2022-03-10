7c6b87e6e4b92bfa7e27372a8b888bce7a7085c4
fankhtob <tobias7402@gmail.com>
2022-03-10 20:10:23 +0100

    last adjustments from merge conflict

--- chessy_agent.py
+++ chessy_agent.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 from chess_env import ChessEnv
-from neural_net import NeuralNet
+from neural_net import SARSA_NN, QLEARNING_NN
+import matplotlib.pyplot as plt
 
 
 class ChessyAgent:
@@ -21,8 +22,32 @@ class ChessyAgent:
 
         # INITALISE YOUR NEURAL NETWORK...
         # HYPERPARAMETERS SUGGESTED (FOR A GRID SIZE OF 4)
-        nn = NeuralNet(self.env, [N_in, N_h1, N_a], xavier=True)
 
-        N_episodes = 200000  # THE NUMBER OF GAMES TO BE PLAYED
+        sarsa = SARSA_NN(self.env, [N_in, N_h1, N_a], xavier=True)
+        qlearning = QLEARNING_NN(self.env, [N_in, N_h1, N_a], xavier=True)
 
-        nn.train(N_episodes, callback)
+        N_episodes = 1000  # THE NUMBER OF GAMES TO BE PLAYED
+
+        name1, reward1, moves1 = sarsa.train(N_episodes, callback)
+        name2, reward2, moves2 = qlearning.train(N_episodes, callback)
+
+        self.print_stats(N_episodes, name1, reward1, moves1, name2, reward2, moves2)
+
+    def print_stats(self, n_episodes, name1, r_save1, step_save1, name2, r_save2,
+                    step_save2):
+        episodes = range(n_episodes)
+        plt.subplots_adjust(wspace=1, hspace=0.3)
+
+        plt.subplot(2, 1, 1)
+        plt.plot(episodes, r_save1, label=name1)
+        plt.plot(episodes, r_save2, label=name2)
+        plt.title(f"Avg. Rewards")
+        plt.legend()
+
+        plt.subplot(2, 1, 2)
+        plt.plot(episodes, step_save1, label=name1)
+        plt.plot(episodes, step_save2, label=name2)
+        plt.title(f"Avg. Steps ")
+        plt.legend()
+
+        plt.show()
--- main.py
+++ main.py
@@ -96,35 +96,7 @@ def perform_nerual_network(env):
 
     # INITALISE YOUR NEURAL NETWORK...
     # HYPERPARAMETERS SUGGESTED (FOR A GRID SIZE OF 4)
-    sarsa = SARSA_NN(env, [N_in, N_h1, N_a], xavier=True)
-    qlearning = QLEARNING_NN(env, [N_in, N_h1, N_a], xavier=True)
 
-    N_episodes = 100  # THE NUMBER OF GAMES TO BE PLAYED
-
-    name1, reward1, moves1 = sarsa.train(N_episodes)
-    name2, reward2, moves2 = qlearning.train(N_episodes)
-
-    print_stats(N_episodes, name1, reward1, moves1, name2, reward2, moves2)
-
-
-def print_stats(n_episodes, name1, r_save1, step_save1, name2, r_save2,
-                step_save2):
-    episodes = range(n_episodes)
-    plt.subplots_adjust(wspace=1, hspace=0.3)
-
-    plt.subplot(2, 1, 1)
-    plt.plot(episodes, r_save1, label=name1)
-    plt.plot(episodes, r_save2, label=name2)
-    plt.title(f"Avg. Rewards")
-    plt.legend()
-
-    plt.subplot(2, 1, 2)
-    plt.plot(episodes, step_save1, label=name1)
-    plt.plot(episodes, step_save2, label=name2)
-    plt.title(f"Avg. Steps ")
-    plt.legend()
-
-    plt.show()
 
 
 if __name__ == '__main__':
