fix backprop input

neuronal_engine/helper.py
@@ -11,8 +11,7 @@ def epsilon_greedy_policy(Qvalues, a, epsilon):
     else:
         a = a[np.argmax(Qvalues[a])]
 
-    qvalue = np.zeros(len(Qvalues))
-    qvalue[a] = Qvalues[a]
+    qvalue = Qvalues[a]
 
     return a, qvalue
 

neuronal_engine/neural_net.py
@@ -7,7 +7,7 @@ from util.logger import log
 
 SARSA = 'sarsa'
 QLEARNING = 'q-learning'
-MAX_STEPS_ALLOWED = 150
+MAX_STEPS_ALLOWED = 500
 
 
 class NeuralNet:
@@ -83,7 +83,7 @@ class NeuralNet:
                     a_agent_next, qvalue_next = epsilon_greedy_policy(
                         x_next[-1], a_next,
                         epsilon_f if self.type == SARSA else 0)
-                    self.prop.backprop(x, a, R, qvalue, Done, qvalue_next)
+                    self.prop.backprop(x, a_agent, R, qvalue, Done, qvalue_next)
 
                 S = np.copy(S_next)
                 X = np.copy(X_next)

neuronal_engine/propagation_handler.py
@@ -16,7 +16,7 @@ class PropagationHandler:
         h = []
         for idx in range(len(weights)):
             h.append(np.dot(weights[idx], x[-1]) + biases[idx])
-            x.append(1 / (1 + np.exp(-h[-1])))
+            x.append(np.maximum(0.1*h[-1], h[-1]))
 
         return x
 